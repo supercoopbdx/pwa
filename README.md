af94b4815d1a9f08d42adc130341b9dd83c507fe
Max <2103462+olibri-us@users.noreply.github.com>
2025-03-17 13:07:13 +0100

    feat(setup): mise en place PWA avec Vite + Vue + Tailwindcss

---
+++ README.md
@@ -0,0 +1,17 @@
+# Supercoop PWA
+
+# Démarrage
+
+## Dév local 
+
+```shell
+$ npm install
+$ npm run dev
+```
+
+# Docs
+
+- [VueJS](https://vuejs.org/guide/)
+- [Vite](https://vite.dev/guide/)
+- [Vite PWA](https://vite-pwa-org.netlify.app/guide/)
+- [Tailwindcss](https://tailwindcss.com/docs/installation/using-vite)
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import PWABadge from './components/PWABadge.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/favicon.svg" class="logo" alt="pwa logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="pwa" />
+  <PWABadge />
+</template>
+
