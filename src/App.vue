e637442a655946c7963e5dad0cc83f7ac9f82329
Max <2103462+olibri-us@users.noreply.github.com>
2025-03-17 19:21:17 +0100

    feat(barcode): test barcode scan component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import PWABadge from './components/PWABadge.vue'
+import { StreamBarcodeReader } from 'vue-barcode-reader'
+
+function onDecode(text) {
+    alert(text)
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/favicon.svg" class="logo" alt="pwa logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="pwa" />
-  <PWABadge />
+    <StreamBarcodeReader @decode="onDecode" />
 </template>
 
