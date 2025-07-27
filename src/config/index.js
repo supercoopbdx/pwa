dcefe2a15c63196db96eb469b1ef89ccc723915c
Andros78 <andresbodenheimer@gmail.com>
2025-07-27 16:22:45 +0200

    change  config

--- src/config/index.js
+++ src/config/index.js
@@ -10,7 +10,7 @@ if (LOCAL) {
 } else {
   config = {    
     api: {
-      baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000',
+      baseURL: import.meta.env.VITE_API_BASE_URL || 'https://backend.supercoop.fr',
     },
   };
 }
